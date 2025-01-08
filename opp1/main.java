515351c237fc57160be346baa68712373e39913c
alseytdastan <dastanalseit@gmail.com>
2025-01-08 22:22:32 +0500

    Update main.java

--- opp1/main.java
+++ opp1/main.java
@@ -1,132 +1,116 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
+        SportsClub club1 = new SportsClub("Astana Warriors", "Astana");
+        SportsClub club2 = new SportsClub("Almaty Tigers", "Almaty");
 
-        Sport football = new Sport("Football", 11);
-        Sport basketball = new Sport("Volleyball", 5);
+        Player player1 = new Player("Altai", 25, "Football", club1);
+        Player player2 = new Player("Aslan", 22, "Basketball", club2);
+        Player player3 = new Player("Almas", 27, "Football", club1);
 
-
-        Player player1 = new Player("Aslan", 21, "Football");
-        Player player2 = new Player("Arman", 19, "Volleyball");
-
-
-        SportsClub club = new SportsClub("Aitu Club");
-
-
-        football.printInfo();
-        basketball.printInfo();
-
+        club1.printInfo();
+        club2.printInfo();
 
         player1.printInfo();
         player2.printInfo();
+        player3.printInfo();
 
 
-        club.printInfo();
-
-
-        if (player1.getAge() > player2.getAge()) {
-            System.out.println(player1.getName() + " is older than " + player2.getName());
-        } else if (player1.getAge() < player2.getAge()) {
-            System.out.println(player2.getName() + " is older than " + player1.getName());
-        } else {
-            System.out.println(player1.getName() + " and " + player2.getName() + " are the same age.");
-        }
-    }
-
-    public static class Player {
-        private String name;
-        private int age;
-        private String sport;
-
-
-        public Player(String name, int age, String sport) {
-            this.name = name;
-            this.age = age;
-            this.sport = sport;
-        }
-
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        public String getSport() {
-            return sport;
-        }
-
-        public void setSport(String sport) {
-            this.sport = sport;
-        }
-
-
-        public void printInfo() {
-            System.out.println("Main.Player Name: " + name + ", Age: " + age + ", Main.Sport: " + sport);
-        }
-    }
-
-    public static class Sport {
-        private String name;
-        private int playersRequired;
-
-
-        public Sport(String name, int playersRequired) {
-            this.name = name;
-            this.playersRequired = playersRequired;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getPlayersRequired() {
-            return playersRequired;
-        }
-
-        public void setPlayersRequired(int playersRequired) {
-            this.playersRequired = playersRequired;
-        }
-
-
-        public void printInfo() {
-            System.out.println("Main.Sport: " + name + ", Players Required: " + playersRequired);
-        }
-    }
-
-    public static class SportsClub {
-        private String name;
-
-
-        public SportsClub(String name) {
-            this.name = name;
-        }
-
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-
-        public void printInfo() {
-            System.out.println("Sports Club Name: " + name);
-        }
+        System.out.println("Are the two clubs the same? " + club1.equals(club2))
+        System.out.println("Does " + player1.getName() + " belong to the same club as " + player3.getName() + "? " + player1.getSportsClub().equals(player3.getSportsClub()));
     }
 }
+
+class SportsClub {
+    private String name;
+    private String location;
+
+    public SportsClub(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void printInfo() {
+        System.out.println("Sports Club: " + this.name);
+        System.out.println("Location: " + this.location);
+        System.out.println();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        SportsClub that = (SportsClub) obj;
+        return this.name.equals(that.name) && this.location.equals(that.location);
+    }
+}
+
+class Player {
+    private String name;
+    private int age;
+    private String sport;
+    private SportsClub sportsClub;
+
+    public Player(String name, int age, String sport, SportsClub sportsClub) {
+        this.name = name;
+        this.age = age;
+        this.sport = sport;
+        this.sportsClub = sportsClub;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSport() {
+        return this.sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public SportsClub getSportsClub() {
+        return this.sportsClub;
+    }
+
+    public void setSportsClub(SportsClub sportsClub) {
+        this.sportsClub = sportsClub;
+    }
+
+    public void printInfo() {
+        System.out.println("Player: " + this.name);
+        System.out.println("Age: " + this.age);
+        System.out.println("Sport: " + this.sport);
+        System.out.println("Sports Club: " + this.sportsClub.getName());
+        System.out.println();
+    }
+}
+
